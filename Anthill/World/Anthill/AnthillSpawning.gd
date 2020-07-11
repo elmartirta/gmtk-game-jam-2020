708d120669ab7e5745a5f9dfc9d7688c2894b943
Maserthous <35930761+Maserthous@users.noreply.github.com>
2020-07-10 20:39:41 -0400

    Revert "Add anthill- broken spawning functionality"
    This reverts commit c6fd2d5801bc404448fb3ae240ec5ec0264c17be.

--- Anthill/World/Anthill/AnthillSpawning.gd
+++ Anthill/World/Anthill/AnthillSpawning.gd
@@ -1,14 +1,6 @@
 extends Node2D
 
-onready var SpawnedAnts : YSort = $"Spawned Ants";
-onready var SpawnTimer : Timer = $"Spawn Timer";
+onready var SpriteCanvas : YSort = $Sprites;
 
-var num_ants = 0
-
-func _on_Spawn_Timer_timeout():
-	spawn_ant()
-
-func spawn_ant():
-	num_ants += 1;
-	var new_ant = load("res://World/Ants/Ants.tscn").instance();
-	SpawnedAnts.add_child(new_ant)
+func _ready():
+	pass # Replace with function body.
