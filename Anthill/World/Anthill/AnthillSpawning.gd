dc88e4e6818a8b5de8a2cb77991458988acca302
Elmar T <elmartirta@rogers.com>
2020-07-11 20:52:36 -0400

    Update anthill spawning to spawn diverse kinds of ants

--- Anthill/World/Anthill/AnthillSpawning.gd
+++ Anthill/World/Anthill/AnthillSpawning.gd
@@ -3,6 +3,13 @@ extends Node2D
 onready var SpawnedAnts : YSort = $"Spawned Ants";
 onready var SpawnTimer : Timer = $"Spawn Timer";
 
+var generic_ant_path = "res://World/Ants/Ants.tscn"
+var scout_ant_path = "res://World/Ants/Scout.tscn"
+var hauler_ant_path = "res://World/Ants/Hauler.tscn"
+var dummy_ant_path = "res://World/Ants/Dummy.tscn"
+var soldier_ant_path = "res://World/Ants/Soldier.tscn"
+
+
 var num_ants = 0
 
 func _on_Spawn_Timer_timeout():
@@ -10,5 +17,34 @@ func _on_Spawn_Timer_timeout():
 
 func spawn_ant():
 	num_ants += 1;
-	var new_ant = load("res://World/Ants/Ants.tscn").instance();
+
+	var new_ant_number = rand_range(0,20);
+	var new_ant_path = "";
+
+	match new_ant_number:
+		20: continue
+		19: continue
+		18: continue
+		17: continue
+		16: continue
+		15: continue
+		14: continue
+		13: continue
+		12: continue
+		11: continue
+		10: continue
+		9: continue
+		8: continue
+		7: new_ant_path = dummy_ant_path
+		6: continue
+		5: continue
+		4: new_ant_path = scout_ant_path
+		3: continue
+		2: continue
+		1: continue
+		0: new_ant_path = soldier_ant_path
+
+
+
+	var new_ant = load(new_ant_path).instance();
 	SpawnedAnts.add_child(new_ant)
