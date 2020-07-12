579821889b4015148abe95ae540924cc55debc40
Maserthous <35930761+Maserthous@users.noreply.github.com>
2020-07-11 20:29:33 -0400

    Starting work different forms of movement

--- Anthill/World/Ants/Ants.gd
+++ Anthill/World/Ants/Ants.gd
@@ -4,17 +4,47 @@ export var SPEED = 10
 export var MIN_TIME = 1
 export var MAX_TIME = 5
 
+enum{
+	WANDER,
+	CHARGE,
+	RETURN
+}
+
 onready var timer = $Timer
+
+var state = WANDER
 
 var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
 
 func _ready():
-	choose_direction()
+	set_state_wander()
 
 func _physics_process(delta):
+	
+	match state:
+		WANDER:
+			pass
+		CHARGE:
+			pass
+		RETURN:
+			pass
+	
+	
+	
+	
 	velocity = direction * SPEED * delta
 	velocity = move_and_collide(velocity)
+
+func set_state_wander():
+	state = WANDER
+	choose_direction()
+
+func set_state_charge():
+	pass
+
+func set_state_return():
+	pass
 
 func choose_direction():
 	direction.x = rand_range(-1, 1)
